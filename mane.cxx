5544a481526dc9f4cdc4a478a23a013fdad0ff62
Валентина Карнаухова <little_scamp@yahoo.com>
2020-07-13 16:41:39 +0000

    pushka

--- mane.cxx
+++ mane.cxx
@@ -3,15 +3,60 @@
 Background::Background(QWidget* parent = 0) : QWidget(parent) {
     setFixedSize(width, height);
     img = new QPixmap("./png/back.png");
-    btn = new QPushButton("suks", this);
+    gun = new QPixmap("./png/pushka.png");
+    curGun = new QPixmap(*gun);
+
+/*    btn = new QPushButton("suks", this);
     btn->setGeometry((width>>1)-40, height-30, 80, 30);
-    QObject::connect(btn, SIGNAL(clicked()), QApplication::instance(), SLOT(quit()));
-    connect(this, SIGNAL(clicked()), this, SLOT(onclick()));
+    QObject::connect(btn, SIGNAL(clicked()), QApplication::instance(), SLOT(quit())); */
 }
 
 void Background::paintEvent(QPaintEvent* event) {
     QPainter painter(this);
     painter.drawPixmap(QRect(0,0, width, height), *img); 
+    painter.drawPixmap(QRect((width - curGun->size().width())>>1,
+                height-(curGun->size().height()>>1), curGun->size().width(), curGun->size().height()), *curGun);
+}
+
+void Background::rotateGun(int x, int y) {
+    using namespace std;
+    x -= width>>1;
+    double tg = (double)x/(double)y;
+    gunAngle = qAtan(tg);
+    cout << "angle: " << gunAngle << ", tg: " << tg << endl;
+    QMatrix rm;
+    rm.rotate(qRadiansToDegrees(gunAngle));
+    delete curGun;
+    curGun = new QPixmap(gun->transformed(rm));
+    update();
+}
+
+void Background::mousePressEvent(QMouseEvent* event) {
+    using namespace std;
+    if (event->button() == Qt::LeftButton) {
+        auto point = event->pos();
+        pressed = true; 
+        cout << "clicked on: " << point.x() << ' ' << point.y() << endl;
+        rotateGun(point.x(), point.y());
+    }
+}
+
+
+void Background::mouseMoveEvent(QMouseEvent* event) {
+    using namespace std;
+    if (pressed) {
+        auto point = event->pos();
+        cout << "moved to: " << point.x() << ' ' << point.y() << endl;
+        rotateGun(point.x(), point.y());
+    }
+}
+
+void Background::mouseReleaseEvent(QMouseEvent* event) {
+    pressed = false;
+}
+
+void Background::resizeEvent(QResizeEvent* event) {
+
 }
 
 void Background::onclick() {
--- mane.h
+++ mane.h
@@ -8,15 +8,28 @@
 #include <QGraphicsItem>
 #include <QSlider>
 #include <QProgressBar>
+#include <QMouseEvent>
+#include <QtMath>
+
 
 struct Background : public QWidget {
     Q_OBJECT
     size_t height = 600, width = 430;
+    bool pressed=false, init=false;
     QPushButton* btn;
     QPixmap* img;
+    QPixmap* gun;
+    QPixmap* curGun;
+    double gunAngle = M_PI_2;
+    void rotateGun(int,int);
 public slots:
     void onclick();
+protected:
+    void mousePressEvent(QMouseEvent* event) override;
+    void paintEvent(QPaintEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
 public:
     Background(QWidget*);
-    void paintEvent(QPaintEvent* event) override;
 };
