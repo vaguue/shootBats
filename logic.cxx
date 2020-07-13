8cae38f28fae60fbc1c6cebd62143b95896ad6d7
Валентина Карнаухова <little_scamp@yahoo.com>
2020-07-13 22:03:51 +0000

    logic

--- logic.cxx
+++ logic.cxx
@@ -1,8 +1,6 @@
 #include "logic.h"
 
-#define F first
-#define S second
-#define mk make_pair
+#define mk make_tuple
 
 Logic::Logic(size_t* w, size_t* h) : w{w}, h{h} {
     bulletTime.setInterval(tout);
@@ -22,24 +20,42 @@ void Logic::initField() {
 }
 
 void Logic::initGun() {
+    start.setX(*w/2);
+    start.setY(0);
     bulletTime.start();
 }
 
 void Logic::shoot(double angle) {
     using namespace std;
-    bullets.push_back(mk(start, 0));
+    bullets.push_back(mk(start, 0, angle));
 }
 
 void Logic::timerEvent(QTimerEvent* event) {
+    using namespace std;
 if (event->timerId() == bulletTime.timerId()) {
+    vector<size_t> dead;
     for (size_t i = 0; i < bullets.size(); ++i) {
         auto& e = bullets[i];
-        e.S += tout;
-        e.F.setY(e.F.y() + )
+        auto& point = get<0>(e);
+        auto x = point.x();
+        auto y = point.y();
+        auto time = get<1>(e);
+        auto angle = get<2>(e);
+        if (field[qCeil(x)][qCeil(y)]) {
+            dead.push_back(i);
+        }
+        else {
+            point.setX(speed * time * qCos(angle));
+            point.setY(speed * time * qSin(angle) - 3*time*time); //TODO
+            get<2>(e) += tout/1000;
+        }
+    }
+    //TODO SIGNAL
+    for (size_t i = 0; i < dead.size(); ++i) {
+        bullets.erase(bullets.begin() + dead[i]);
+        //TODO SIGNAL
     }
 }
 }
 
-#undef F
-#undef S
 #undef mk
--- logic.h
+++ logic.h
@@ -2,17 +2,19 @@
 #include <vector>
 #include <utility>
 #include <QTimer>
+#include <QPoint>
 #include <QRandomGenerator>
 #include <QtMath>
+#include <QTimerEvent>
 
 struct Logic {
     std::vector<std::vector<bool>> field; 
-    std::vector<std::<QPoint, int>> bullets;
+    std::vector<std::tuple<QPointF, double, double>> bullets;
     int tout = 70;
     int speed = 1;
     size_t* h;
     size_t* w;
-    QPoint start;
+    QPointF start;
     QTimer bulletTime;
     Logic(size_t* h, size_t* w);
     void initField();
